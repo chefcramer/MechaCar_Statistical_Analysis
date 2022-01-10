e37feb463e4391373c83a23816d3695889e3d620
Ryan Cramer <chefcramer@gmail.com>
2022-01-09 22:40:14 -0700

    Challenge deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,7 +3,7 @@
 library(dplyr)
 
 
-# read in the csv file
+# read in the MechaCar csv file
 cars_df <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
 
 #perform linear regression using lm(), passing all 6 variables and the dataframe as the data paramater
@@ -11,3 +11,12 @@ lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD
 
 #use summary() to determine the p-value and the r squared value
 summary(lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=cars_df))
+
+# read in the Suspension coil csv file
+coil_df <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+
+#make a total summary dataframe using the summarize function to get the mean, median, variance, and standard dev of the PSI columns
+total_summary <- coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI),.groups='keep')
+
+#create lot_summary dataframe using group by and the summarize functions to group each lot my mean median variance and SD of the PSI column
+lot_summary <- coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI),.groups='keep')
